8a9047c3de799e31d66cdc22e8b5780044c8db8f
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2024-07-16 16:57:12 +0500

    make main structure of website

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+    
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/routes/Routers.jsx
@@ -0,0 +1,30 @@
+import {Routes,Route,Navigate} from "react-router-dom";
+
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import SignUp from "../pages/Signup";
+import Checkout from "../pages/Checkout";
+import ProductDetails from "../pages/ProductDetails";
+
+
+const Routers = () => {
+
+  return <Routes>
+
+    <Route path="/" element={<Navigate to="home"/>}/>
+    <Route path="home" element={<Home/>}/>
+    <Route path="shop" element={<Shop/>}/>
+    <Route path="shop/:id" element={<ProductDetails/>}/>
+    <Route path="cart" element={<Cart/>}/>
+    <Route path="checkout" element={<Checkout/>}/>
+    <Route path="login" element={<Login/>}/>
+    <Route path="signup" element={<SignUp/>}/>
+    
+  </Routes>
+
+}
+
+export default Routers
