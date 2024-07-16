f85eeee6b89929d070cb706ff6e65274a63541e7
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2024-07-16 18:24:43 +0500

    add some icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./header.css";
+import { Container, Row } from "reactstrap";
+// import images
+import Logo from "../../assets/images/eco-logo.png";
+import userIcon from "../../assets/images/user-icon.png"
 
 const Header = () => {
-    
   return (
-    <div>
-      <p>Header</p>
-    </div>
-  )
-}
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper">
+            <div className="logo">
+              <img src={Logo} alt="logo_image" />
+              <div>
+                <h1>OXUN MARKET</h1>
+                <p>Since 1990</p>
+              </div>
+            </div>
+            <div className="navigation">
+              <ul className="menu">
+                <li className="nav__item">
+                  <NavLink to="home">Home</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="shop">Shop</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="cart">Cart</NavLink>
+                </li>
+              </ul>
+            </div>
 
-export default Header
+            <div className="nav__icons">
+              <span className="fav__icon"><i className="ri-heart-line"></i></span>
+              <span className="cart__icon"><i className="ri-shopping-bag-line"></i></span>
+              <span><img src={userIcon} alt="user_icon" /></span>
+            </div>
+
+            <div className="mobile__menu">
+              <span>
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
+
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
